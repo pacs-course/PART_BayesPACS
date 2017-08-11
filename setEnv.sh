c1a6b4150d7e5e934e6880a081c67762aa7d60aa
vagrant <vagrant@localhost.localdomain>
2017-08-11 09:01:14 +0000

    setting environment upload

---
+++ setEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## USER-DEFINED SECTION
+export mkArmadilloLib=/home/vagrant/armadillo-7.800.2/build
+export mkArpackLib=/u/sw/pkgs/toolchains/gcc-glibc/5/pkgs/arpack/3.3.0/lib
+export mkOpenblasLib=/u/sw/pkgs/toolchains/gcc-glibc/5/pkgs/openblas/0.2.17/lib
+export mkSuperluLib=/u/sw/pkgs/toolchains/gcc-glibc/5/pkgs/superlu/5/lib
+export mkArmadilloInc=/home/vagrant/armadillo-7.800.2/include
+export mkOpenblasInc=/u/sw/pkgs/toolchains/gcc-glibc/5/pkgs/openblas/0.2.17/include
+export mkSuperluInc=/u/sw/pkgs/toolchains/gcc-glibc/5/pkgs/superlu/5/include
+## END USER-DEFINED SECTION
